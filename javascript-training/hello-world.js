5d1231672901e09fce4ec79723eee0284c3a22a8
Jyoti Srivastava <jyotieduacc@gmail.com>
2026-06-24 21:39:51 +0530

    Assignments

---
+++ javascript-training/hello-world.js
@@ -0,0 +1,12 @@
+console.log("Hello, World");
+
+//console.log is an print statement that prints the info.
+
+//1. Education and learning purpose.
+//2.Debugging and error detection.
+//3.Monitoring the program education to understand the flow.
+//4. Testing and validating the results.
+//5.Log the system results to analyze later.
+
+//1. single line comment. => starts with // (shortcut(command+/))
+//2. multi-line comments. => starts with /* and ends with */   (shortcut(command+shift+/))
